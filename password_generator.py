03853d8face605ecf74e24e47a3892dedbcff2c4
Han <89624707+Fhanafi@users.noreply.github.com>
2023-01-27 08:02:07 +0700

    update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,5 @@
 #password generator
 import string,secrets
-
 karakter = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(secrets.choice(karakter) for i in range(12))
 print("Password kamu = " + password)
